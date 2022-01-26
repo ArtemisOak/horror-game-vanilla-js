fc803ff150b8ff2fdea2ce0fcd890ea34059459e
ArtemisOak <thompsonaj4@s.dcsdk12.org>
2022-01-26 16:24:28 -0700

    Player class beginnings

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,35 @@ const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+class Player {
+	constructor() {
+		this.width = 32;
+		this.height = 32;
+		this.x = canvas.width / 2 - this.width / 2;
+		this.y = canvas.height / 2 - this.height / 2;
+		this.wireUpEvents();
+	}
+	wireUpEvents() {
+		window.addEventListener("keydown", (e) => {});
+	}
+	update() {}
+	render() {
+		ctx.save();
+		ctx.fillStyle = "green";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.restore();
+	}
+}
+
+let player = new Player();
+
+function gameLoop() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	player.update();
+	player.render();
+
+	requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
